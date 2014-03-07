aedd4fdf028d32efb5a0dc0917e04601367d3402
Mark Greene <mark.greene@yahoo.com>
2014-03-06 22:30:04 -0500

    added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@ Post.create(url: "google.com", title: "My first post", description: "This is my 
 Post.create(url: "google.com", title: "My second post", description: "This is my second post", user_id: User.first.id )
 Comment.create(body: "This is my first comment", user_id: User.first.id, post_id: Post.first.id )
 Comment.create(body: "This is my second comment", user_id: User.first.id, post_id: Post.find(2).id )
+Comment.create(body: "This is my third comment", user_id: User.first.id, post_id: Post.find(2).id )
 PostCategory.create( post_id: Post.first.id, category_id: Category.first.id )
 PostCategory.create( post_id: Post.first.id, category_id: Category.find(2).id )
+PostCategory.create( post_id: Post.find(2).id, category_id: Category.find(3).id )
