22896a9655d27c08b2fc1859150939b613320755
Mark Greene <mark.greene@yahoo.com>
2014-03-08 16:04:03 -0500

    Lesson 2 - Added categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,22 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new params.require(:category).permit(:name)
+
+    if @category.save
+      flash[:notice] = 'Your category was successfully created'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+end
