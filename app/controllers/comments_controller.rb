ab8e595c0e8b8c86006b1ee5badbfb1cdb33bedd
Mark Greene <mark.greene@yahoo.com>
2014-03-20 23:05:40 -0400

    Added voting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,4 +15,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def vote
+    @comment = Comment.find(params[:id])
+    vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+
+    if vote.valid?
+      flash[:notice] = "Your vote was counted."
+    else
+      flash[:error] = "You can only vote once."
+    end
+
+    redirect_to :back
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319015303) do
+ActiveRecord::Schema.define(version: 20140320204734) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -55,5 +55,16 @@ ActiveRecord::Schema.define(version: 20140319015303) do
     t.datetime "updated_at"
     t.string   "password_digest"
   end
+
+  create_table "votes", force: true do |t|
+    t.boolean  "vote"
+    t.integer  "user_id"
+    t.integer  "voteable_id"
+    t.string   "voteable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["voteable_id", "voteable_type"], name: "index_votes_on_voteable_id_and_voteable_type"
 
 end
