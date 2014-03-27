c43be0ca7247d740f8df28229a027caeacba5153
Mark Greene <mark.greene@yahoo.com>
2014-03-26 20:28:54 -0400

    added votable and sluggable modules

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module PostitTemplate
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # autoload everything in the lib directory
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
